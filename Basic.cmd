819d9f1fd69551cdb34b969037ca70df51d7a8e8
mirage335 <spamfreemirage335@gmail.com>
2013-06-01 00:28:36 +0000

    first commit

---
+++ Basic.cmd
@@ -0,0 +1,44 @@
+# Pin name action command file
+
+# Start of element D1
+ChangePinName(D1, 1, 1)
+ChangePinName(D1, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, 2, G)
+ChangePinName(Q1, 3, D)
+ChangePinName(Q1, 1, S)
+
+# Start of element CONN3
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element T1
+ChangePinName(T1, 3, 3)
+ChangePinName(T1, 4, 4)
+ChangePinName(T1, 1, 1)
+ChangePinName(T1, 2, 2)
