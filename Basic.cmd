c0e55c164a91879087d8c91cc99f76bc0222d4a1
mirage335 <spamfreemirage335@gmail.com>
2013-06-02 03:36:15 +0000

    Progress.

--- Basic.cmd
+++ Basic.cmd
@@ -1,21 +1,33 @@
 # Pin name action command file
 
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 1, 1)
+ChangePinName(D2, 2, 2)
+
 # Start of element D1
 ChangePinName(D1, 1, 1)
 ChangePinName(D1, 2, 2)
 
 # Start of element Q1
-ChangePinName(Q1, 2, G)
-ChangePinName(Q1, 3, D)
-ChangePinName(Q1, 1, S)
+ChangePinName(Q1, 2, 2)
+ChangePinName(Q1, 1, 1)
+ChangePinName(Q1, 3, 3)
 
 # Start of element CONN3
 ChangePinName(CONN3, 1, 1)
 ChangePinName(CONN3, 2, 2)
-
-# Start of element C1
-ChangePinName(C1, 2, 2)
-ChangePinName(C1, 1, 1)
 
 # Start of element CONN2
 ChangePinName(CONN2, 1, 1)
@@ -25,17 +37,17 @@ ChangePinName(CONN2, 2, 2)
 ChangePinName(CONN1, 1, 1)
 ChangePinName(CONN1, 2, 2)
 
-# Start of element C3
-ChangePinName(C3, 2, 2)
-ChangePinName(C3, 1, 1)
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
 
 # Start of element R1
 ChangePinName(R1, 1, 1)
 ChangePinName(R1, 2, 2)
 
-# Start of element C2
-ChangePinName(C2, 2, 2)
-ChangePinName(C2, 1, 1)
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
 
 # Start of element T1
 ChangePinName(T1, 3, 3)
